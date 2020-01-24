ffa5b8ce45216e25e377230a853405ec4c9dcbde
Gerald Oa <onegerrald@gmail.com>
2020-01-24 17:56:04 +0000

    close of work 24-jan-2020

---
+++ projects-js/JS_part_two/DOM_tree.js
@@ -0,0 +1,12 @@
+/** 
+ * DOM TREE
+ * The backbone of an HTML document is tags.
+
+According to the Document Object Model (DOM), every HTML tag is an object.
+Nested tags are “children” of the enclosing one. The text inside a tag is an object as well.
+
+All these objects are accessible using JavaScript, and we can use them to modify the page.
+
+For example, document.body is the object representing the <body> tag.
+
+Running this code will make the <body> red for 3 seconds:*/
